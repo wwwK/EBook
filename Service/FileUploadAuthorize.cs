a82917bad07f01daeeb6164725b85ef330511f4f
yh <yh0xa55a8@vip.qq.com>
2019-07-08 19:08:35 +0800

    Complete file service and file controller demo. Add AvatarPath attribute to Customer Model

---
+++ Service/FileUploadAuthorize.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Text;
+using System.Web.Helpers;
+using System.Security.Cryptography;
+
+namespace EBook.Service
+{
+    public static class FileUploadAuthorize
+    {
+        private struct UploadPolicy
+        {
+            public string Scope;
+            public long Deadline;
+        }
+
+        private const string BucketName = "sparkxyf";
+        private const string AccessKey = "QGoBQa9YkgBvuExS8bonspckkWTWKaS9KXbVmoV8";
+        private const string SecretKey = "ibms3XH0QazGPjuxquvdPPnkvhbxvHVABCoIJxhs";
+        
+        private static string GenerateUpdatePolicy(string fileName)
+        {
+            var policy = new UploadPolicy()
+            {
+                Scope = $"{BucketName}:{fileName}",
+                Deadline = new DateTimeOffset(DateTime.Now.AddMinutes(10)).ToUnixTimeSeconds()
+            };
+
+            return Json.Encode(policy);
+        }
+
+        private static string WebSafeBase64Encode(string text)
+        {
+            return WebSafeBase64Encode(Encoding.UTF8.GetBytes(text));
+        }
+
+        private static string WebSafeBase64Encode(byte[] data)
+        {
+            return Convert.ToBase64String(data).Replace('+', '-').Replace('/', '_');
+        }
+        
+        public static string GenerateUploadToken(string fileName)
+        {
+            var policy = GenerateUpdatePolicy(fileName);
+
+            var encodedPolicy = WebSafeBase64Encode(policy);
+            // 编码上传策略  得到待编码字符串
+        
+            var signTool = new HMACSHA1(Encoding.UTF8.GetBytes(SecretKey));
+            var sign = signTool.ComputeHash(Encoding.UTF8.GetBytes(encodedPolicy));
+            // 使用访问密钥签名
+        
+            var encodedSign = WebSafeBase64Encode(sign);
+            // 编码签名
+        
+            return $"{AccessKey}:{encodedSign}:{encodedPolicy}";
+        }
+    }
+}
--- Service/Session.cs
+++ Service/Session.cs
@@ -6,20 +6,44 @@ namespace EBook.Service
 {
     public static class Session
     {
+        private class SessionItem
+        {
+            public int UserId;
+            public DateTime Expire;
+        }
+        
         // session->userId
-        private static readonly Dictionary<int, int> SessionMap = new Dictionary<int, int>();
+        private static readonly Dictionary<int, SessionItem> SessionMap = new Dictionary<int, SessionItem>();
 
         public static int SetSessionId(int userId)
         {
             var random = new Random();
             var sessionId = random.Next();
-            SessionMap.Add(sessionId, userId);
+            SessionMap.Add(sessionId, new SessionItem()
+            {
+                UserId = userId,
+                Expire = DateTime.Now.AddHours(1)
+            });
             return sessionId;
         }
 
+        // :return: userId:success, -1: Session not found, -2: session expired
         public static int GetUserIdFromSession(int session)
         {
-            return SessionMap.ContainsKey(session) ? SessionMap[session] : 0;
+            if (SessionMap.ContainsKey(session))
+            {
+                if (SessionMap[session].Expire < DateTime.Now)
+                {
+                    SessionMap.Remove(session);
+                    return -2;
+                }
+                SessionMap[session].Expire = DateTime.Now.AddHours(1);
+                return SessionMap[session].UserId;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
