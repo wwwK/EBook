e676fbc8effca31abc0a099a1cb22fdcf4dc158c
鸽子王小笼包 <461304186@qq.com>
2019-07-10 22:28:59 +0800

    add complex search api

--- Controllers/BookSearchController.cs
+++ Controllers/BookSearchController.cs
@@ -22,10 +22,18 @@ using System.Threading.Tasks;
          public class SearchDate
          {
              public readonly string Title;
+             public readonly string Author;
+             public readonly string Publisher;
+             public readonly string ISBN;
+             public readonly string ShopName;
 
-             public SearchDate(string title)
+             public SearchDate(string title, string author, string publisher, string isbn, string shopName)
              {
                  Title = title;
+                 Author = author;
+                 Publisher = publisher;
+                 ISBN = isbn;
+                 ShopName = shopName;
              }
          }
 
@@ -42,11 +50,36 @@ using System.Threading.Tasks;
              {
                  return BadRequest(ModelState);
              }
- 
+
+             int changed = 0;
+             BookInfo[] books = BookSearch.BookSearchWithTitle("");
+             if (data.Title != null)
+             {
+                 changed = 1;
+                 books = books.Intersect(BookSearch.BookSearchWithTitle(data.Title)).ToArray();
+             }
+             if (data.Author != null)
+             {
+                 changed = 1;
+                 books = books.Intersect(BookSearch.BookSearchWithAuther(data.Author)).ToArray();
+             }
+             if (data.Publisher != null)
+             {
+                 changed = 1;
+                 books = books.Intersect(BookSearch.BookSearchWithPublisher(data.Publisher)).ToArray();
+             }
+             if (data.ISBN != null)
+             {
+                 changed = 1;
+                 books = books.Intersect(BookSearch.BookSearchWithIsnb(data.ISBN)).ToArray();
+             }
+             if (data.Title != null)
+             {
+                 changed = 1;
+                 books = books.Intersect(BookSearch.BookSearchWithShopName(data.ShopName)).ToArray();
+             }
              
-             //var a = new BookSearch();
-             BookInfo[] books = BookSearch.BookSearchWithTitle(data.Title);
-             if (books.Length == 0)
+             if (books.Length == 0 || changed == 0)
              {
                  return BadRequest("没有找到书籍！");
              }
